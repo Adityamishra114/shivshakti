89633a3369c61fdb468ce8f7eb87004e7eab5f27
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-01-04 20:00:15 +0530

    change fix

--- wp-content/themes/blocksy/functions.php
+++ wp-content/themes/blocksy/functions.php
@@ -14,3 +14,28 @@ if (version_compare(PHP_VERSION, '5.7.0', '<')) {
 
 require get_template_directory() . '/inc/init.php';
 
+function custom_sticky_header_script() {
+    ?>
+    <script>
+        jQuery(document).ready(function($) {
+            var header = $('.ekit-template-content-markup.ekit-template-content-header.ekit-template-content-theme-support');
+            var prevScrollPos = $(window).scrollTop();
+
+            $(window).scroll(function() {
+                var currentScrollPos = $(window).scrollTop();
+
+                if (prevScrollPos > currentScrollPos) {
+                    // Scrolling up, make the background white
+                    header.css('background-color', '#ffffff');
+                } else {
+                    // Scrolling down, make the background transparent
+                    header.addClass('transparent-bg');
+                }
+
+                prevScrollPos = currentScrollPos;
+            });
+        });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'custom_sticky_header_script');
